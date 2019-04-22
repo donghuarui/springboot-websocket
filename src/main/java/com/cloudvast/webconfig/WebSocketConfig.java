dc8b79f973d8c7004309f9211248fc3f88581ccd
董华锐 <ezreal.ma@foxmail.com>
2019-04-22 11:35:20 +0800

    Initial commit

---
+++ src/main/java/com/cloudvast/webconfig/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.cloudvast.webconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+
+/**
+ * 开启WebSocket支持
+ * @author donghuarui
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
